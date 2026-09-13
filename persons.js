23e73975a42b583583dbd854b001b2293cce925e
Eshaan <singapuri.eshaan@gmail,com>
2026-09-13 15:07:39 -0700

    woww amazingg

--- persons.js
+++ persons.js
@@ -83,7 +83,7 @@ app.post('/api/persons', (request, response) => {
     response.json(phon)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
